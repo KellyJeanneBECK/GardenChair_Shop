1636a6678fe92a1110695b82e727d3a4613a545c
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2026-02-27 11:46:42 +0100

    change user role user manager page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -19,6 +19,24 @@ final class UserController extends AbstractController
         ]);
     }
 
+    #[Route('/admin/user/edit/{id}', name: 'app_user_edit')]
+    public function editUser(User $user, EntityManagerInterface $entityManager): Response
+    {   
+        $userRole = $user->getRoles();
+
+        if (in_array('ROLE_EDITOR', $userRole)) {
+            $user->setRoles( array('ROLE_USER') );
+            $entityManager->flush();
+            $this->addFlash('danger', 'The user is not an editor anymore');
+        } else {
+            $user->setRoles( array('ROLE_EDITOR', 'ROLE_USER') );
+            $entityManager->flush();
+            $this->addFlash('success', 'The user is now an editor');
+        }
+
+        return $this->redirectToRoute('app_user');
+    }
+
     #[Route('/admin/user/delete/{id}', name: 'app_user_delete')]
     public function deleteUser(User $user, EntityManagerInterface $entityManager): Response
     {
