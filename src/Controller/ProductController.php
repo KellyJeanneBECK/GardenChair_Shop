60db4141368fe1258173ef6d4adc612d2bfb2596
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2026-03-03 16:04:11 +0100

    start product history

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\AddProductHistory;
 use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -51,6 +53,13 @@ final class ProductController extends AbstractController
             }
 
             $entityManager->persist($product);
+            $entityManager->flush();
+
+            $stockHistory = new AddProductHistory();
+            $stockHistory->setQuantity($product->getStock());
+            $stockHistory->setProduct($product);
+            $stockHistory->setCreatedAt(new DateTimeImmutable());
+            $entityManager->persist($stockHistory);
             $entityManager->flush();
 
             $this->addFlash('success', 'Your product was created with success');
