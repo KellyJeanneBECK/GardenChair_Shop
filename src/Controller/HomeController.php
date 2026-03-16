7a77331e861c09b44bf0ba3cfaf6a94beaf07256
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2026-03-16 11:23:47 +0100

    filter product with subCategory

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use App\Repository\SubCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -28,6 +29,19 @@ final class HomeController extends AbstractController
         return $this->render('home/show.html.twig', [
             'product' => $product,
             'productList' => $productRepository->findBy([], ['id'=>'DESC'], 4),
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/home/subcategory/{id}/filter', name:'app_home_product_filter')]
+    public function filter($id, SubCategoryRepository $subCategoryRepository, CategoryRepository $categoryRepository): Response
+    {
+        $product = $subCategoryRepository->find($id)->getProducts();
+        $subCategory = $subCategoryRepository->find($id);
+
+        return $this->render('home/filter.html.twig', [
+            'products' => $product,
+            'subCategory' => $subCategory,
             'categories' => $categoryRepository->findAll(),
         ]);
     }
