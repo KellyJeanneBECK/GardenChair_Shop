705c2ba1ea5dac996e098e187bc1f4a6f6793167
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2026-03-25 09:10:49 +0100

    print order pdf

--- src/Controller/BillController.php
+++ src/Controller/BillController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,8 +18,21 @@ final class BillController extends AbstractController
     {
         $order = $orderRepo->find($id);
 
-        return $this->render('bill/index.html.twig', [
-            'order' => $order,
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $domPdf = new Dompdf($pdfOptions);
+        $html = $this->renderView('bill/index.html.twig', [
+            'order' => $order
+        ]);
+        $domPdf->loadHtml($html);
+        $domPdf->render();
+        $domPdf->stream('GardenChair-Bill-'.$order->getId().'.pdf', [
+            'Attachment' => false
+        ]);
+        exit;
+
+        return new Response('', 200, [
+            'Content-Type' => 'application/pdf',
         ]);
     }
 }
